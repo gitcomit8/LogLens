7279ec918b59b69753100a24e65530036e9d34bd
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-09-27 20:52:13 +0530

    feat: Implement JSON parsing engine
    - This commit introduces JSON deserialization using the 'serde_json' crate
    - A 'ParsedLine' struct has been defined to represent a structured log entry.
    - The main application logic is updated to parse each line from stdin into this struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use serde::Deserialize;
+use serde_json::Value;
+use std::collections::BTreeMap;
 use std::io::{self, BufRead};
 
+#[derive(Debug, Deserialize)]
+struct ParsedLine {
+    //Option<String> is used because these fields might not be in
+    //every log line
+    level: Option<String>,
+    message: Option<String>,
+
+    //this macro tells serde to put all other fields
+    //from the JSON object into this map
+    #[serde(flatten)]
+    rest: BTreeMap<String, Value>,
+}
 fn main() {
     //handle to standard input stream
     let stdin = io::stdin();
@@ -9,9 +24,14 @@ fn main() {
         //Each line is a Result<String,Error>
         //Use match to handle both Ok and Err
         match line {
-            Ok(l) => {
-                println!("{}", l);
-            }
+            Ok(l) => match serde_json::from_str::<ParsedLine>(&l) {
+                Ok(parsed_line) => {
+                    println!("{:?}", parsed_line);
+                }
+                Err(e) => {
+                    eprintln!("Failed to parse JSON: {}", e);
+                }
+            },
             Err(e) => {
                 eprintln!("Error reading line: {}", e);
             }
