4dd10dccd35d6f23f05a2f6216bbc9db834f67bb
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-09-27 20:43:37 +0530

    feat: Implement initial stdin ingestion engine
    - This commit establishes the foundational data pipeline
    - This fulfulls the Day 1 goal of the Roadmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    //handle to standard input stream
+    let stdin = io::stdin();
+
+    //Lock the handle for exclusive access
+    for line in stdin.lock().lines() {
+        //Each line is a Result<String,Error>
+        //Use match to handle both Ok and Err
+        match line {
+            Ok(l) => {
+                println!("{}", l);
+            }
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+            }
+        }
+    }
 }
