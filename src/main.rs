8e3c4890b4206b48af6b928d1cec2c6d9c49d825
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-09-27 22:20:25 +0530

    feat: Add TUI boilerplate and main application loop
    - This commit replaces the previos stdin based logic with a full interactive application structure
    
    - This completes the Day 3 goal of the roadmap

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,78 @@
-use serde::Deserialize;
-use serde_json::Value;
-use std::collections::BTreeMap;
-use std::io::{self, BufRead};
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode};
+use crossterm::terminal::{
+	disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use crossterm::{event, execute};
+use ratatui::backend::{Backend, CrosstermBackend};
+use ratatui::layout::{Alignment, Constraint, Layout};
+use ratatui::prelude::Direction;
+use ratatui::widgets::{Block, Borders, Paragraph};
+use ratatui::{Frame, Terminal};
+use std::io;
 
-#[derive(Debug, Deserialize)]
-struct ParsedLine {
-    //Option<String> is used because these fields might not be in
-    //every log line
-    level: Option<String>,
-    message: Option<String>,
-
-    //this macro tells serde to put all other fields
-    //from the JSON object into this map
-    #[serde(flatten)]
-    rest: BTreeMap<String, Value>,
+struct App {
+    should_quit: bool,
 }
-fn main() {
-    //handle to standard input stream
-    let stdin = io::stdin();
+fn main() -> io::Result<()> {
+    enable_raw_mode()?; //Puts terminal to handle key presses directly
+    let mut stdout = io::stdout();
+    //Switches to alternate screen buffer to
+    // prevent messing up shell history
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    //Lock the handle for exclusive access
-    for line in stdin.lock().lines() {
-        //Each line is a Result<String,Error>
-        //Use match to handle both Ok and Err
-        match line {
-            Ok(l) => match serde_json::from_str::<ParsedLine>(&l) {
-                Ok(parsed_line) => {
-                    println!("{:?}", parsed_line);
+    //Create app and run it
+    let app = App { should_quit: false };
+    run_tui(&mut terminal, app)?;
+
+    //Restore terminal to original state
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+//Main TUI loop
+fn run_tui<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        //draw the ui
+        terminal.draw(|f| ui(f, &app))?;
+
+        //handle events like key presses
+        // poll with a timeout of 250ms
+        if event::poll(std::time::Duration::from_millis(250))? {
+            if let Event::Key(key) = event::read()? {
+                if key.code == KeyCode::Char('q') {
+                    app.should_quit = true;
                 }
-                Err(e) => {
-                    eprintln!("Failed to parse JSON: {}", e);
-                }
-            },
-            Err(e) => {
-                eprintln!("Error reading line: {}", e);
             }
         }
+        if app.should_quit {
+            return Ok(());
+        }
     }
+}
+
+//this fn defines and draws the widgets
+fn ui(frame: &mut Frame, _app: &App) {
+    //Create main layout that splits the screen vertically
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .split(frame.area());
+
+    //Create paragraph widget
+    let text = Paragraph::new("Hello, LogLens!\n Press 'q' to quit.")
+        .block(Block::default().borders(Borders::ALL).title("LogLens"))
+        .alignment(Alignment::Center);
+
+    //Render the widget in the first chunk of the layout
+    frame.render_widget(text, chunks[0]);
 }
